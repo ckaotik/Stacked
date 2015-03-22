1f6830f4133d91a92dc8a2c21a0a462aff63598b
ckaotik <ckaotik@gmail.com>
2015-03-22 16:21:28 +0100

    Updated LibAddonMenu-2.0

--- libs/LibAddonMenu-2.0/LibAddonMenu-2.0.lua
+++ libs/LibAddonMenu-2.0/LibAddonMenu-2.0.lua
@@ -1,16 +1,34 @@
-﻿--	LibAddonMenu-2.0 & its files © Ryan Lakanen (Seerah)	--
---	All Rights Reserved										--
---	Permission is granted to use Seerah's LibAddonMenu-2.0	--
---	in your project. Any modifications to LibAddonMenu-2.0	--
---	may not be redistributed.								--
---------------------------------------------------------------
+﻿--	LibAddonMenu-2.0 & its files © Ryan Lakanen (Seerah)		--
+--	Distributed under The Artistic License 2.0 (see LICENSE)	--
+------------------------------------------------------------------
 
 
 --Register LAM with LibStub
-local MAJOR, MINOR = "LibAddonMenu-2.0", 7
+local MAJOR, MINOR = "LibAddonMenu-2.0", 17
 local lam, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
-if not lam then return end	--the same or newer version of this lib is already loaded into memory 
+if not lam then return end	--the same or newer version of this lib is already loaded into memory
 
+local messages = {}
+local MESSAGE_PREFIX = "[LAM2] "
+local function PrintLater(msg)
+	if(CHAT_SYSTEM.primaryContainer) then
+		d(MESSAGE_PREFIX .. msg)
+	else
+		messages[#messages + 1] = msg
+	end
+end
+
+local function FlushMessages()
+	for i = 1, #messages do
+		d(MESSAGE_PREFIX .. messages[i])
+	end
+	messages = {}
+end
+
+if(LAMSettingsPanelCreated and not LAMCompatibilityWarning) then
+	PrintLater("An old version of LibAddonMenu with compatibility issues was detected. For more information on how to proceed search for LibAddonMenu on esoui.com")
+	LAMCompatibilityWarning = true
+end
 
 --UPVALUES--
 local wm = WINDOW_MANAGER
@@ -22,7 +40,8 @@ local _
 local addonsForList = {}
 local addonToOptionsMap = {}
 local optionsCreated = {}
-local widgets = {}
+lam.widgets = lam.widgets or {}
+local widgets = lam.widgets
 
 
 --METHOD: REGISTER WIDGET--
@@ -47,18 +66,31 @@ end
 --METHOD: OPEN TO ADDON PANEL--
 --opens to a specific addon's option panel
 --Usage:
---	panel = userdata; the panel returned by the :RegisterOptionsPanel method
---local settings = {en = "Settings", de = "Einstellungen", fr = "Réglages"}
---local locSettings = settings[GetCVar("Language.2")]
+--  panel = userdata; the panel returned by the :RegisterOptionsPanel method
 local locSettings = GetString(SI_GAME_MENU_SETTINGS)
 function lam:OpenToPanel(panel)
 	SCENE_MANAGER:Show("gameMenuInGame")
 	zo_callLater(function()
-			ZO_GameMenu_InGame.gameMenu.headerControls[locSettings]:SetOpen(true)
-			SCENE_MANAGER:AddFragment(OPTIONS_WINDOW_FRAGMENT)
-			ZO_OptionsWindow_ChangePanels(lam.panelID)
-			panel:SetHidden(false)
-		end, 200)
+		local settingsMenu = ZO_GameMenu_InGame.gameMenu.headerControls[locSettings]
+		settingsMenu:SetOpen(true)
+		SCENE_MANAGER:AddFragment(OPTIONS_WINDOW_FRAGMENT)
+		KEYBOARD_OPTIONS:ChangePanels(lam.panelID)
+		for i, child in pairs(settingsMenu.children) do
+			if type(child) == "table" and child.data.name == KEYBOARD_OPTIONS.panelNames[lam.panelID] then
+				ZO_TreeEntry_OnMouseUp(child.control, true)
+				break
+			end
+		end
+		local scroll = LAMAddonPanelsMenuScrollChild
+		for i = 1, scroll:GetNumChildren() do
+			local button = scroll:GetChild(i)
+			if button.panel == panel then
+				zo_callHandler(button, "OnClicked")
+				ZO_Scroll_ScrollControlToTop(LAMAddonPanelsMenu, button)
+				break
+			end
+		end
+	end, 200)
 end
 
 
@@ -68,62 +100,75 @@ end
 local function CreateOptionsControls(panel)
 	local addonID = panel:GetName()
 	local optionsTable = addonToOptionsMap[addonID]
-	
+
 	if optionsTable then
-		local lastAddedControl, lacAtHalfRow
-		for _, widgetData in ipairs(optionsTable) do
-			local widgetType = widgetData.type
+		local isHalf, widget
+		local lastAddedControl, lacAtHalfRow, oIndex, widgetData, widgetType
+		local submenu, subWidgetData, sIndex, subWidgetType, subWidget
+		local anchorOffset = 0
+		local anchorOffsetSub
+		local lastAddedControlSub, lacAtHalfRowSub
+		for oIndex=1,#optionsTable do
+			widgetData = optionsTable[oIndex]
+			widgetType = widgetData.type
 			if widgetType == "submenu" then
-				local submenu = LAMCreateControl[widgetType](panel, widgetData)
+				submenu = LAMCreateControl[widgetType](panel, widgetData)
 				if lastAddedControl then
-					submenu:SetAnchor(TOPLEFT, lastAddedControl, BOTTOMLEFT, 0, 15)
+					submenu:SetAnchor(TOPLEFT, lastAddedControl, BOTTOMLEFT, 0, 15 + anchorOffset)
 				else
 					submenu:SetAnchor(TOPLEFT)
 				end
 				lastAddedControl = submenu
 				lacAtHalfRow = false
-				
-				local lastAddedControlSub, lacAtHalfRowSub
-				for _, subWidgetData in ipairs(widgetData.controls) do
-					local subWidgetType = subWidgetData.type
-					local subWidget = LAMCreateControl[subWidgetType](submenu, subWidgetData)
-					local isHalf = subWidgetData.width == "half"
+
+				anchorOffsetSub = 0
+				lacAtHalfRowSub = nil
+				lastAddedControlSub = nil
+				for sIndex=1,#widgetData.controls do
+					subWidgetData = widgetData.controls[sIndex]
+					subWidgetType = subWidgetData.type
+					subWidget = LAMCreateControl[subWidgetType](submenu, subWidgetData)
+					isHalf = subWidgetData.width == "half"
 					if lastAddedControlSub then
 						if lacAtHalfRowSub and isHalf then
 							subWidget:SetAnchor(TOPLEFT, lastAddedControlSub, TOPRIGHT, 5, 0)
 							lacAtHalfRowSub = false
+							anchorOffsetSub = zo_max(0, subWidget:GetHeight() - lastAddedControlSub:GetHeight())
 						else
-							subWidget:SetAnchor(TOPLEFT, lastAddedControlSub, BOTTOMLEFT, 0, 15)
-							lacAtHalfRowSub = isHalf and true or false
+							subWidget:SetAnchor(TOPLEFT, lastAddedControlSub, BOTTOMLEFT, 0, 15 + anchorOffsetSub)
+							lacAtHalfRowSub = isHalf
+							anchorOffsetSub = 0
 							lastAddedControlSub = subWidget
 						end
 					else
 						subWidget:SetAnchor(TOPLEFT)
-						lacAtHalfRowSub = isHalf and true or false
+						lacAtHalfRowSub = isHalf
 						lastAddedControlSub = subWidget
 					end
 				end
 			else
-				local widget = LAMCreateControl[widgetType](panel, widgetData)
-				local isHalf = widgetData.width == "half"
+				widget = LAMCreateControl[widgetType](panel, widgetData)
+				isHalf = widgetData.width == "half"
 				if lastAddedControl then
 					if lacAtHalfRow and isHalf then
 						widget:SetAnchor(TOPLEFT, lastAddedControl, TOPRIGHT, 10, 0)
+						anchorOffset = zo_max(0, widget:GetHeight() - lastAddedControl:GetHeight())
 						lacAtHalfRow = false
 					else
-						widget:SetAnchor(TOPLEFT, lastAddedControl, BOTTOMLEFT, 0, 15)
-						lacAtHalfRow = isHalf and true or false
+						widget:SetAnchor(TOPLEFT, lastAddedControl, BOTTOMLEFT, 0, 15 + anchorOffset)
+						lacAtHalfRow = isHalf
+						anchorOffset = 0
 						lastAddedControl = widget
 					end
 				else
 					widget:SetAnchor(TOPLEFT)
-					lacAtHalfRow = isHalf and true or false
+					lacAtHalfRow = isHalf
 					lastAddedControl = widget
 				end
 			end
-		end	
+		end
 	end
-	
+
 	optionsCreated[addonID] = true
 	cm:FireCallbacks("LAM-PanelControlsCreated", panel)
 end
@@ -137,14 +182,16 @@ local function ToggleAddonPanels(panel)	--called in OnShow of newly shown panel
 		currentlySelected:SetHidden(true)
 	end
 	LAMAddonPanelsMenu.currentlySelected = panel
-	
+
 	if not optionsCreated[panel:GetName()] then	--if this is the first time opening this panel, create these options
 		CreateOptionsControls(panel)
 	end
-	
+
 	cm:FireCallbacks("LAM-RefreshPanel", panel)
 end
 
+local Initialize
+local hasInitialized = false
 
 --METHOD: REGISTER ADDON PANEL
 --registers your addon with LibAddonMenu and creates a panel
@@ -152,6 +199,7 @@ end
 --	addonID = "string"; unique ID which will be the global name of your panel
 --	panelData = table; data object for your panel - see controls\panel.lua
 function lam:RegisterAddonPanel(addonID, panelData)
+	if(not hasInitialized) then Initialize(addonID) end
 	local panel = lamcc.panel(nil, panelData, addonID)	--addonID==global name of panel
 	panel:SetHidden(true)
 	panel:SetAnchor(TOPLEFT, LAMAddonPanelsMenu, TOPRIGHT, 10, 0)
@@ -165,7 +213,7 @@ function lam:RegisterAddonPanel(addonID, panelData)
 			lam:OpenToPanel(panel)
 		end
 	end
-	
+
 	return panel	--return for authors creating options manually
 end
 
@@ -192,7 +240,7 @@ local dummyFunc = function() end
 local panelWindow = ZO_OptionsWindow
 local bgL = ZO_OptionsWindowBGLeft
 local bgR = ZO_OptionsWindowBGLeftBGRight
-local function HandlePanelSwitching(panel)
+local function HandlePanelSwitching(self, panel)
 	if panel == lam.panelID then	--our addon settings panel
 		oldDefaultButton:SetCallback(dummyFunc)
 		oldDefaultButton:SetHidden(true)
@@ -218,14 +266,23 @@ end
 local function CreateAddonSettingsPanel()
 	if not LAMSettingsPanelCreated then
 		local controlPanelID = "LAM_ADDON_SETTINGS_PANEL"
-		local controlPanelNames = {en = "Addon Settings", fr = "Extensions", de = "Erweiterungen"}
+		--Russian for TERAB1T's RuESO addon, which creates an "ru" locale
+		--game font does not support Cyrillic, so they are using custom fonts + extended latin charset
+		--Spanish provided by Luisen75 for their translation project
+		local controlPanelNames = {
+			en = "Addon Settings",
+			fr = "Extensions",
+			de = "Erweiterungen",
+			ru = "Îacòpoéêè äoïoìîeîèé",
+			es = "Configura Addons",
+		}
 
-		ZO_OptionsWindow_AddUserPanel(controlPanelID, controlPanelNames[GetCVar("Language.2")])
+		ZO_OptionsWindow_AddUserPanel(controlPanelID, controlPanelNames[GetCVar("Language.2")] or controlPanelNames["en"], PANEL_TYPE_SETTINGS)
 
 		lam.panelID = _G[controlPanelID]
-		
-		ZO_PreHook("ZO_OptionsWindow_ChangePanels", HandlePanelSwitching)
-		
+
+		ZO_PreHook(ZO_KeyboardOptions, "ChangePanels", HandlePanelSwitching)
+
 		LAMSettingsPanelCreated = true
 	end
 end
@@ -235,14 +292,14 @@ end
 --adds each registered addon to the menu in LAM's panel
 local function CreateAddonButtons(list, addons)
 	for i = 1, #addons do
-		local button = wm:CreateControlFromVirtual("LAMAddonMenuButton"..i, list, "ZO_DefaultTextButton")
+		local button = wm:CreateControlFromVirtual("LAMAddonMenuButton"..i, list.scrollChild, "ZO_DefaultTextButton")
 		button.name = addons[i].name
 		button.panel = _G[addons[i].panel]
 		button:SetText(button.name)
 		button:SetHorizontalAlignment(TEXT_ALIGN_LEFT)
 		button:SetWidth(190)
 		if i == 1 then
-			button:SetAnchor(TOPLEFT, list, TOPLEFT, 5, 5)
+			button:SetAnchor(TOPLEFT, list.scrollChild, TOPLEFT, 5, 5)
 		else
 			button:SetAnchor(TOPLEFT, _G["LAMAddonMenuButton"..i-1], BOTTOMLEFT)
 		end
@@ -265,8 +322,11 @@ local function CreateAddonList()
 	list.bg = list.bg or wm:CreateControl(nil, list, CT_BACKDROP)
 	local bg = list.bg
 	bg:SetAnchorFill()	--offsets of 8?
-	bg:SetEdgeTexture("EsoUI\\Art\\Tooltips\\UI-Border.dds", 128, 16)
+	bg:SetEdgeTexture("EsoUI\\Art\\miscellaneous\\borderedinsettransparent_edgefile.dds", 128, 16)
 	bg:SetCenterColor(0, 0, 0, 0)
+
+	list.scrollChild = LAMAddonPanelsMenuScrollChild
+	list.scrollChild:SetResizeToFitPadding(0, 15)
 
 	local generatedButtons
 	list:SetHandler("OnShow", function(self)
@@ -283,17 +343,42 @@ local function CreateAddonList()
 			end
 			if self.currentlySelected then self.currentlySelected:SetHidden(false) end
 		end)
-	
-	list.controlType = OPTIONS_CUSTOM
-	list.panel = lam.panelID
-	
+
+	--list.controlType = OPTIONS_CUSTOM
+	--list.panel = lam.panelID
+	list.data = {
+		controlType = OPTIONS_CUSTOM,
+		panel = lam.panelID,
+	}
+
 	ZO_OptionsWindow_InitializeControl(list)
 
 	return list
 end
 
-
 --INITIALIZING
-CreateAddonSettingsPanel()
-CreateAddonList()
+local safeToInitialize = false
 
+local eventHandle = table.concat({MAJOR, MINOR}, "r")
+local function OnLoad(_, addonName)
+	-- wait for the first loaded event
+	EVENT_MANAGER:UnregisterForEvent(eventHandle, EVENT_ADD_ON_LOADED)
+	safeToInitialize = true
+end
+EVENT_MANAGER:RegisterForEvent(eventHandle, EVENT_ADD_ON_LOADED, OnLoad)
+
+local function OnActivated(_, addonName)
+	EVENT_MANAGER:UnregisterForEvent(eventHandle, EVENT_PLAYER_ACTIVATED)
+	FlushMessages()
+end
+EVENT_MANAGER:RegisterForEvent(eventHandle, EVENT_PLAYER_ACTIVATED, OnActivated)
+
+function Initialize(addonID)
+	if(not safeToInitialize) then
+		local msg = string.format("The panel with id '%s' was registered before addon loading has completed. This might break the AddOn Settings menu.", addonID)
+		PrintLater(msg)
+	end
+	CreateAddonSettingsPanel()
+	CreateAddonList()
+	hasInitialized = true
+end
